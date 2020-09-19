ca84d4a5c1f89dcff927db4b119a75e5027c0283
Vlad <Vlad@KOMP>
2020-09-19 18:26:56 +0300

    Minor UI changes.

--- WF_Miner/MainForm.Designer.cs
+++ WF_Miner/MainForm.Designer.cs
@@ -28,166 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this._menuStrip = new System.Windows.Forms.MenuStrip();
-            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.loadGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this._bottomStatusStrip = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this._txtElapsedTime = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
-            this._txtMinesLeft = new System.Windows.Forms.ToolStripStatusLabel();
-            this._mainPanel = new System.Windows.Forms.Panel();
-            this._menuStrip.SuspendLayout();
-            this._bottomStatusStrip.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // _menuStrip
-            // 
-            this._menuStrip.BackColor = System.Drawing.Color.OrangeRed;
-            this._menuStrip.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+         this._menuStrip = new System.Windows.Forms.MenuStrip();
+         this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.loadGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this._bottomStatusStrip = new System.Windows.Forms.StatusStrip();
+         this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+         this._txtElapsedTime = new System.Windows.Forms.ToolStripStatusLabel();
+         this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+         this._txtMinesLeft = new System.Windows.Forms.ToolStripStatusLabel();
+         this._mainPanel = new System.Windows.Forms.Panel();
+         this._menuStrip.SuspendLayout();
+         this._bottomStatusStrip.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // _menuStrip
+         // 
+         this._menuStrip.BackColor = System.Drawing.Color.OrangeRed;
+         this._menuStrip.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this._menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.gameToolStripMenuItem});
-            this._menuStrip.Location = new System.Drawing.Point(0, 0);
-            this._menuStrip.Name = "_menuStrip";
-            this._menuStrip.Size = new System.Drawing.Size(284, 24);
-            this._menuStrip.TabIndex = 0;
-            // 
-            // gameToolStripMenuItem
-            // 
-            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+         this._menuStrip.Location = new System.Drawing.Point(0, 0);
+         this._menuStrip.Name = "_menuStrip";
+         this._menuStrip.Size = new System.Drawing.Size(284, 24);
+         this._menuStrip.TabIndex = 0;
+         // 
+         // gameToolStripMenuItem
+         // 
+         this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.newGameToolStripMenuItem,
             this.optionsToolStripMenuItem,
             this.saveGameToolStripMenuItem,
             this.loadGameToolStripMenuItem,
             this.aboutToolStripMenuItem,
             this.exitToolStripMenuItem});
-            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
-            this.gameToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
-            this.gameToolStripMenuItem.Text = "Game";
-            // 
-            // newGameToolStripMenuItem
-            // 
-            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
-            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
-            this.newGameToolStripMenuItem.Text = "New game";
-            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
-            // 
-            // optionsToolStripMenuItem
-            // 
-            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
-            this.optionsToolStripMenuItem.Text = "Options";
-            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
-            // 
-            // saveGameToolStripMenuItem
-            // 
-            this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
-            this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
-            this.saveGameToolStripMenuItem.Text = "Save game";
-            this.saveGameToolStripMenuItem.Click += new System.EventHandler(this.saveGameToolStripMenuItem_Click);
-            // 
-            // loadGameToolStripMenuItem
-            // 
-            this.loadGameToolStripMenuItem.Name = "loadGameToolStripMenuItem";
-            this.loadGameToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
-            this.loadGameToolStripMenuItem.Text = "Load game";
-            this.loadGameToolStripMenuItem.Click += new System.EventHandler(this.loadGameToolStripMenuItem_Click);
-            // 
-            // aboutToolStripMenuItem
-            // 
-            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
-            this.aboutToolStripMenuItem.Text = "About";
-            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
-            // _bottomStatusStrip
-            // 
-            this._bottomStatusStrip.BackColor = System.Drawing.Color.OrangeRed;
-            this._bottomStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+         this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+         this.gameToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+         this.gameToolStripMenuItem.Text = "Game";
+         // 
+         // newGameToolStripMenuItem
+         // 
+         this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+         this.newGameToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+         this.newGameToolStripMenuItem.Text = "New game";
+         this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+         // 
+         // optionsToolStripMenuItem
+         // 
+         this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+         this.optionsToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+         this.optionsToolStripMenuItem.Text = "Options";
+         this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+         // 
+         // saveGameToolStripMenuItem
+         // 
+         this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
+         this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+         this.saveGameToolStripMenuItem.Text = "Save game";
+         this.saveGameToolStripMenuItem.Click += new System.EventHandler(this.saveGameToolStripMenuItem_Click);
+         // 
+         // loadGameToolStripMenuItem
+         // 
+         this.loadGameToolStripMenuItem.Name = "loadGameToolStripMenuItem";
+         this.loadGameToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+         this.loadGameToolStripMenuItem.Text = "Load game";
+         this.loadGameToolStripMenuItem.Click += new System.EventHandler(this.loadGameToolStripMenuItem_Click);
+         // 
+         // aboutToolStripMenuItem
+         // 
+         this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+         this.aboutToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+         this.aboutToolStripMenuItem.Text = "About";
+         this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+         // 
+         // exitToolStripMenuItem
+         // 
+         this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+         this.exitToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+         this.exitToolStripMenuItem.Text = "Exit";
+         this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+         // 
+         // _bottomStatusStrip
+         // 
+         this._bottomStatusStrip.BackColor = System.Drawing.Color.OrangeRed;
+         this._bottomStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this._txtElapsedTime,
             this.toolStripStatusLabel3,
             this._txtMinesLeft});
-            this._bottomStatusStrip.Location = new System.Drawing.Point(0, 294);
-            this._bottomStatusStrip.Name = "_bottomStatusStrip";
-            this._bottomStatusStrip.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this._bottomStatusStrip.Size = new System.Drawing.Size(284, 22);
-            this._bottomStatusStrip.TabIndex = 1;
-            // 
-            // toolStripStatusLabel1
-            // 
-            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(41, 17);
-            this.toolStripStatusLabel1.Text = "Time:";
-            // 
-            // _txtElapsedTime
-            // 
-            this._txtElapsedTime.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._txtElapsedTime.Name = "_txtElapsedTime";
-            this._txtElapsedTime.Size = new System.Drawing.Size(15, 17);
-            this._txtElapsedTime.Text = "0";
-            // 
-            // toolStripStatusLabel3
-            // 
-            this.toolStripStatusLabel3.Font = new System.Drawing.Font("Showcard Gothic", 9F);
-            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
-            this.toolStripStatusLabel3.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.toolStripStatusLabel3.Size = new System.Drawing.Size(50, 17);
-            this.toolStripStatusLabel3.Text = "Mines:";
-            // 
-            // _txtMinesLeft
-            // 
-            this._txtMinesLeft.Font = new System.Drawing.Font("Showcard Gothic", 9F);
-            this._txtMinesLeft.Name = "_txtMinesLeft";
-            this._txtMinesLeft.Size = new System.Drawing.Size(15, 17);
-            this._txtMinesLeft.Text = "0";
-            // 
-            // _mainPanel
-            // 
-            this._mainPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this._mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._mainPanel.Location = new System.Drawing.Point(0, 24);
-            this._mainPanel.Name = "_mainPanel";
-            this._mainPanel.Size = new System.Drawing.Size(284, 270);
-            this._mainPanel.TabIndex = 2;
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(284, 316);
-            this.Controls.Add(this._mainPanel);
-            this.Controls.Add(this._bottomStatusStrip);
-            this.Controls.Add(this._menuStrip);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MainMenuStrip = this._menuStrip;
-            this.MaximizeBox = false;
-            this.Name = "MainForm";
-            this.Text = "Miner 2.0";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
-            this._menuStrip.ResumeLayout(false);
-            this._menuStrip.PerformLayout();
-            this._bottomStatusStrip.ResumeLayout(false);
-            this._bottomStatusStrip.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+         this._bottomStatusStrip.Location = new System.Drawing.Point(0, 294);
+         this._bottomStatusStrip.Name = "_bottomStatusStrip";
+         this._bottomStatusStrip.RightToLeft = System.Windows.Forms.RightToLeft.No;
+         this._bottomStatusStrip.Size = new System.Drawing.Size(284, 22);
+         this._bottomStatusStrip.TabIndex = 1;
+         // 
+         // toolStripStatusLabel1
+         // 
+         this.toolStripStatusLabel1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+         this.toolStripStatusLabel1.Size = new System.Drawing.Size(41, 17);
+         this.toolStripStatusLabel1.Text = "Time:";
+         // 
+         // _txtElapsedTime
+         // 
+         this._txtElapsedTime.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this._txtElapsedTime.Name = "_txtElapsedTime";
+         this._txtElapsedTime.Size = new System.Drawing.Size(15, 17);
+         this._txtElapsedTime.Text = "0";
+         // 
+         // toolStripStatusLabel3
+         // 
+         this.toolStripStatusLabel3.Font = new System.Drawing.Font("Showcard Gothic", 9F);
+         this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+         this.toolStripStatusLabel3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+         this.toolStripStatusLabel3.Size = new System.Drawing.Size(50, 17);
+         this.toolStripStatusLabel3.Text = "Mines:";
+         // 
+         // _txtMinesLeft
+         // 
+         this._txtMinesLeft.Font = new System.Drawing.Font("Showcard Gothic", 9F);
+         this._txtMinesLeft.Name = "_txtMinesLeft";
+         this._txtMinesLeft.Size = new System.Drawing.Size(15, 17);
+         this._txtMinesLeft.Text = "0";
+         // 
+         // _mainPanel
+         // 
+         this._mainPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+         this._mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+         this._mainPanel.Location = new System.Drawing.Point(0, 24);
+         this._mainPanel.Name = "_mainPanel";
+         this._mainPanel.Size = new System.Drawing.Size(284, 270);
+         this._mainPanel.TabIndex = 2;
+         // 
+         // MainForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.AutoSize = true;
+         this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+         this.ClientSize = new System.Drawing.Size(284, 316);
+         this.Controls.Add(this._mainPanel);
+         this.Controls.Add(this._bottomStatusStrip);
+         this.Controls.Add(this._menuStrip);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+         this.MainMenuStrip = this._menuStrip;
+         this.MaximizeBox = false;
+         this.Name = "MainForm";
+         this.Text = "Miner";
+         this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+         this._menuStrip.ResumeLayout(false);
+         this._menuStrip.PerformLayout();
+         this._bottomStatusStrip.ResumeLayout(false);
+         this._bottomStatusStrip.PerformLayout();
+         this.ResumeLayout(false);
+         this.PerformLayout();
 
         }
 
